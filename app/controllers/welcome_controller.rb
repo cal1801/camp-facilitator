b886ed593d1fa8320ea4a390005a3c247d868256
cal1801 <cal.findeiss@pricebook.digital>
2018-09-25 15:23:56 -0400

    Init accounts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,10 @@
 class WelcomeController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :check_account
+
+  private
+  def check_account
+    if current_user.account.nil?
+      redirect_to new_account_path
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  enum role: [:user, :camp_admin, :master_admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  belongs_to :account
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
